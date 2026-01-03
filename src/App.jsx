1431df633b1f06186f1a3009479b323d8e0c41a7
Akhil Kanawade <yashkanawade66@gmail.com>
2026-01-03 20:59:13 +0530

    Fix routing basename and vite config for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Weather from "./Weather"
 function App() {
   return (
     <>
-       <Weather />
+      <Weather />
     </>
   )
 }
--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -32,7 +32,7 @@ export default function SearchBox({ updateInfo }) {
     };
 
 
-    let handleChange = (event) => {
+    let handleChange = (event) =>{
         setcity(event.target.value);
     }
 
